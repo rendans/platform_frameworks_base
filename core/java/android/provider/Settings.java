f17234cd6ac5af8ac0968fe35986d95534b52b57
0xD34D <clark@scheffsblend.com>
2014-02-23 14:20:37 -0300

    Base:screen recorder [1/2] (WIP)
    @alviteri: Ported from KK crDroid Branch
    
    Change-Id: I6cb19910022f1f903a16d5e65f75bd667550917e
    Signed-off-by: iceandfire <arhamjamal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3288,6 +3288,30 @@ public final class Settings {
         public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
 
         /**
+        * Width and height of output vide expressed in WxH
+        * @hide
+        */
+        public static final String SCREEN_RECORDER_OUTPUT_DIMENSIONS = "screen_recorder_output_dimensions";
+
+        /**
+        * Screen recorder framerate in bits per second
+        * @hide
+        */
+        public static final String SCREEN_RECORDER_BITRATE = "screen_recorder_bitrate";
+
+        /**
+        * Whether power menu screen record is enabled
+        * @hide
+        */
+        public static final String POWER_MENU_SCREENRECORD_ENABLED = "power_menu_screenrecord_enabled";
+
+        /**
+        * Whether to include audio when recording a video
+        * @hide
+        */
+        public static final String SCREEN_RECORDER_RECORD_AUDIO = "screen_recorder_record_audio";
+
+        /**
          * Automatic keyboard rotation timeout.  0 to disable completely.
          * @hide
          */
