7fb4ffae84fb1aacc67c5624e69f31eb60a5a8cf
BigBrother1984 <stevewatersy@gmail.com>
2014-04-10 18:07:26 +0200

    Forward Port: Floating Window with Multitasking.
    Forward Port and Fixups done by BlackDragon && rogersb11
    
    Floating window contributors:
    @drcmda
    @beerbong
    @D4rKn3sSyS
    
    This feature, has ability to:
    1. Moveable window to various position
    2. Scaling window with 2 finger like zoom in/out
    3. Double tap in actionbar or above window to fullscreen or back to floating window
    4. Automatically resize when Snapping into one of side (Top, Bottom, Left, Right)
       (taken from xhalofloatingwindow by @zst123)
    5. Automatically switch focus from other app (taken from multiwindow by @xplodwild)
    6. Unlimited floating window app (depends on RAM)
    
    To make app launch using Floating window feature
    
    you must add Intent.FLAG_FLOATING_WINDOW
    before start an activity
    
    for example you can look at https://gerrit.omnirom.org/6515
    
    need https://gerrit.omnirom.org/#/c/6561
    
    PS.17 : fix scaling
    PS.18 : fix layout calculation when rotate
    PS.19 : support minimize/maximize window (see App circle sidebar)
    PS.20 : fix previouse app
    PS.21 : rebased due to changes in 4.4.3_r1.1
    PS.26 : rebased
    PS.27 : rebased
    PS.30 : a little touch :)
    PS.31 : fix FCs because package manager has died
    
    Fusion:
    
    PS.32 : Adapted to work on lollipop.
    PS.33 : Kill floating activities running in the same thread when tapping outside.
    PS.34 : Recents: Launch as Floating App.
    PS.35 : Added some tinted code by XplodWild.
    PS.36 : Remove whitespace.
    PS.37 : Added floating window intent to slimactions.
    PS.38 : Cleanup unused code.
    
    Rogersb11: Add appsidebar intent, slim recents intent and fix bug
    
    Todo: add more intents for some features to allow open in floating window.
    
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>
    Signed-off-by: rogersb11 <brettrogers11@gmail.com>
    Slim actions floating mode switch 2/2
    
    Change-Id: I94c2f29bb4c920b6cf894b59d43f79ef659a5988
    
    Signed-off-by: rogersb11 <brettrogers11@gmail.com>
    
    Floating window update.
    
    Signed-off-by: BlackDragon <blackdragon.fusionteam@gmail.com>
    
    Change-Id: I0dab65e32783e52b556811f9e70b046aac8f65c2
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/java/android/view/IWindowManager.aidl
    	core/java/com/android/internal/util/slim/Action.java
    	core/res/res/values/xenonhd_symbols.xml
    	packages/SystemUI/res/values/strings_aicp.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/AppSidebar.java

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -1896,7 +1896,7 @@ public final class ActivityStackSupervisor implements DisplayListener {
                             }
                             options = null;
                             movedToFront = true;
-                        }
+                        }             
                     }
                     // If the caller has requested that the target task be
                     // reset, then do so.
