767a8f322713d3f0d065621052a3d6af8def361b
mqiu <quic_mqiu@codeaurora.org>
2014-04-22 16:36:27 +0800

    audio: no notification sound when adjusting volume bar twice.
    Notification sound won't take effect when the previous track hasn't
    finished.
    Need explicitly stop the previous ongoing track to alow the current one.
    
    Change-Id: Ib344a8b14ecc47a5c8fdd9c11ae48a3541ea9a55
    CRs-Fixed: 652236

--- core/java/android/preference/VolumePreference.java
+++ core/java/android/preference/VolumePreference.java
@@ -380,6 +380,9 @@ public class VolumePreference extends SeekBarDialogPreference implements
         }
 
         public void onStartTrackingTouch(SeekBar seekBar) {
+            if (isSamplePlaying()) {
+                stopSample();
+            }
         }
 
         public void onStopTrackingTouch(SeekBar seekBar) {
