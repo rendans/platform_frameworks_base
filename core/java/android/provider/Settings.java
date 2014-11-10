c06e16d070e1b37f1b971b21ade29aa19d88e109
Roman Birg <roman@cyngn.com>
2014-11-09 19:52:34 -0800

    SystemUI: add quick settings pull down with one finger
    Squashed:
    SystemUI: fix right hand side gestures on lockscreen
    https://github.com/CyanogenMod/android_frameworks_base/commit/5ff043df1d78d175953a1d773968d6759ef33c1a
    
    SystemUI: add Settings.System toggle for quick-quick settings pulldown
    https://github.com/CyanogenMod/android_frameworks_base/commit/50eab7e3313691826188ccda2fad7bf69a909203
    
    Logic is currently set to use 1/3 of right hand side of the statusbar to
    trigger a quick settings quick pull down, quickly.
    
    Change-Id: I3f536496f724f07d2419a5b2db5fa8f8261609e7
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    * Adapted for Marshmallow by Kreach3R
    https://github.com/TeamTwisted/platform_frameworks_base/commit/9279f83672d9f332291c0e19beb6887b1ddbe23f
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3722,6 +3722,12 @@ public final class Settings {
          */
         public static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
+		/** Whether to allow one finger quick settings expansion on the right side of the statusbar.
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
