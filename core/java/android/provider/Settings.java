51c2d948c4ec7d42bf4e0e89e79f39852b582aea
Lars Greiss <kufikugel@googlemail.com>
2014-12-10 17:55:07 +0100

    Keyguard: Enable quick unlock (1/2)
    old way does not work anymore exactly like before. So adapt the idea to new LP code
    
    Thankd DVTonder for the basic idea prior LP
    
    PS:
    - do PIN view correctly
    
    Change-Id: I44e10670255b12effc524d6291f37e2af15d01ec
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5379,6 +5379,12 @@ public final class Settings {
         public static final String SMS_DEFAULT_APPLICATION = "sms_default_application";
 
         /**
+        * Whether to use the custom quick unlock screen control
+        * @hide
+        */
+        public static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "lockscreen_quick_unlock_control";
+
+        /**
          * Name of a package that the current user has explicitly allowed to see all of that
          * user's notifications.
          *
@@ -5541,7 +5547,8 @@ public final class Settings {
             MOUNT_UMS_NOTIFY_ENABLED,
             UI_NIGHT_MODE,
             SLEEP_TIMEOUT,
-            PRIVACY_GUARD_DEFAULT
+            PRIVACY_GUARD_DEFAULT,
+            LOCKSCREEN_QUICK_UNLOCK_CONTROL
         };
 
         /**
