93ffc8d76928639a7efbd3eb8f509475f39fd87a
Adnan Begovic <adnan@cyngn.com>
2015-01-20 12:47:42 -0800

    Move qs brightness slider setting to secure.
    Change-Id: I05facd68f8f12c6347b175c3adf2d731415926d2
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -73,7 +73,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // database gets upgraded properly. At a minimum, please confirm that 'upgradeVersion'
     // is properly propagated through your change.  Not doing so will result in a loss of user
     // settings.
-    private static final int DATABASE_VERSION = 117;
+    private static final int DATABASE_VERSION = 118;
 
     private Context mContext;
     private int mUserHandle;
@@ -1877,6 +1877,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             moveSettingsToNewTable(db, TABLE_SYSTEM, TABLE_SECURE,
                     qsTiles, true);
             upgradeVersion = 117;
+        }
+
+        if (upgradeVersion < 118) {
+            String[] settingsToMove = new String[] {
+                    Settings.Secure.QS_SHOW_BRIGHTNESS_SLIDER,
+            };
+
+            moveSettingsToNewTable(db, TABLE_SYSTEM, TABLE_SECURE,
+                    settingsToMove, true);
+            upgradeVersion = 118;
         }
 
         // *** Remember to update DATABASE_VERSION above!
