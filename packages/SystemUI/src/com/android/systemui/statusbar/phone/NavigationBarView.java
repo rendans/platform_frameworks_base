ceb0f6f920f5b6bae6365d61463ed9659fa0b277
iceandfire <arhamjamal@gmail.com>
2015-01-23 21:13:36 +0530

    Revert "Navbar: un0@% themechooser"
    This reverts commit 95799ff422180022414611d134291c2d0a15d39c.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -97,6 +97,9 @@ public class NavigationBarView extends LinearLayout {
     int mDisabledFlags = 0;
     int mNavigationIconHints = 0;
 
+    private BackButtonDrawable mBackIcon, mBackLandIcon;
+    private Drawable mHomeIcon, mHomeLandIcon;
+
     private NavigationBarViewTaskSwitchHelper mTaskSwitchHelper;
     private DelegateViewHelper mDelegateHelper;
     private DeadZone mDeadZone;
@@ -235,6 +238,8 @@ public class NavigationBarView extends LinearLayout {
         final Resources res = getContext().getResources();
         final ContentResolver cr = mContext.getContentResolver();
 
+        getIcons(res);
+
         mBarSize = res.getDimensionPixelSize(R.dimen.navigation_bar_size);
         mVertical = false;
         mShowMenu = false;
@@ -341,25 +346,21 @@ public class NavigationBarView extends LinearLayout {
         return mCurrentView.findViewWithTag(constant);
     }
 
+    private void getIcons(Resources res) {
+        mBackIcon = new BackButtonDrawable(res.getDrawable(R.drawable.ic_sysbar_back));
+        mBackLandIcon = new BackButtonDrawable(res.getDrawable(R.drawable.ic_sysbar_back_land));
+        mHomeIcon = res.getDrawable(R.drawable.ic_sysbar_home);
+        mHomeLandIcon = res.getDrawable(R.drawable.ic_sysbar_home_land);
+    }
+
     public void updateResources(Resources res) {
         mThemedResources = res;
+        getIcons(mThemedResources);
         mBarTransitions.updateResources(res);
         for (int i = 0; i < mRotatedViews.length; i++) {
             ViewGroup container = (ViewGroup) mRotatedViews[i];
             if (container != null) {
-                updateKeyButtonViewResources(container);
                 updateLightsOutResources(container);
-            }
-        }
-    }
-
-    private void updateKeyButtonViewResources(ViewGroup container) {
-        if (mCurrentView == null) return;
-        for (final String k : NavbarConstants.NavbarActions()) {
-            final View child = mCurrentView.findViewWithTag(k);
-
-            if (child instanceof KeyButtonView) {
-                ((KeyButtonView) child).setImage(mThemedResources);
             }
         }
     }
@@ -385,6 +386,7 @@ public class NavigationBarView extends LinearLayout {
 
     @Override
     public void setLayoutDirection(int layoutDirection) {
+        getIcons(mThemedResources != null ? mThemedResources : getContext().getResources());
         super.setLayoutDirection(layoutDirection);
     }
 
@@ -398,6 +400,8 @@ public class NavigationBarView extends LinearLayout {
     }
 
     public void setNavigationIconHints(int hints, boolean force) {
+
+        ((ImageView)getHomeButton()).setImageDrawable(mVertical ? mHomeLandIcon : mHomeIcon);
 
         if (!force && hints == mNavigationIconHints) return;
         showingIME = (hints & StatusBarManager.NAVIGATION_HINT_BACK_ALT) != 0;
