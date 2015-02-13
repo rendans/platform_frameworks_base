d21cd0ca4fbfd3dbff0db92392dab83deae925fa
TheBr0ken <jtoro2716@gmail.com>
2015-02-13 18:19:27 -0500

    Revert "WindowManagerService: Fix On-Screen keyboard popups when hw-keyboard is present"
    This reverts commit 35648341865928d2c050698c15a4d486aaa589b8.
    
    This commit broke virtual keyboard on Note devices

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -7261,6 +7261,7 @@ public class WindowManagerService extends IWindowManager.Stub
             }
             if (hardKeyboardAvailable != mHardKeyboardAvailable) {
                 mHardKeyboardAvailable = hardKeyboardAvailable;
+                mShowImeWithHardKeyboard = hardKeyboardAvailable;
                 mH.removeMessages(H.REPORT_HARD_KEYBOARD_STATUS_CHANGE);
                 mH.sendEmptyMessage(H.REPORT_HARD_KEYBOARD_STATUS_CHANGE);
             }
