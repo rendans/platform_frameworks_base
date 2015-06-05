f51f6788c1f5436a3dadf139a05de2616551c7d4
iceandfire <arhamjamal@gmail.com>
2015-06-05 20:25:26 +0530

    Revert "Fixed NAVBAR_POWER behavior"
    This reverts commit db783c4c0206fba1b123342c40a2f592f47f3b92.
    
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
    
    Change-Id: Ib9fa07940e1e3776c96a2c1844a6873d8e0db12b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -77,14 +77,6 @@ public class NavigationBarView extends LinearLayout {
     private OnTouchListener mHomeSearchActionListener;
     private OnLongClickListener mRecentsBackListener;
 
-    private OnLongClickListener mPowerListener = new View.OnLongClickListener() {
-        @Override
-        public boolean onLongClick(View v) {
-            ((KeyButtonView) v).sendEvent(KeyEvent.KEYCODE_POWER, KeyEvent.FLAG_LONG_PRESS);
-            return true;
-        }
-    };
-
     final Display mDisplay;
     View mCurrentView = null;
     View[] mRotatedViews = new View[4];
@@ -805,11 +797,6 @@ public class NavigationBarView extends LinearLayout {
         View homeView = mCurrentView.findViewWithTag(NavbarEditor.NAVBAR_HOME);
         if (homeView != null) {
             homeView.setOnTouchListener(mHomeSearchActionListener);
-        }
-        View powerView = mCurrentView.findViewWithTag(NavbarEditor.NAVBAR_POWER);
-        if (powerView != null) {
-            powerView.setLongClickable(true);
-            powerView.setOnLongClickListener(mPowerListener);
         }
     }
 
