56a71b405a102ecec91dcfb408efdd1cde62c4fb
LorDClockaN <davor@losinj.com>
2015-07-26 14:18:48 +0200

    FWB: Headset icon switch (1/2)
    Change-Id: I561cf99935f9c1ddd0264640dc928bb29d45bdc5
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4009,6 +4009,12 @@ public final class Settings {
          */
         public static final String RECENT_CARD_TEXT_COLOR = "recent_card_text_color";
 
+		/**
+         * Switch for headset icon
+         * @hide
+         */
+        public static final String SHOW_HEADSET_ICON = "show_headset_icon";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
