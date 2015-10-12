9bc475e0b84d8a64f91e2e18d53916a0e334c345
tiger_huang <tiger_huang@htc.com>
2015-10-12 15:24:37 +0800

    Re-use orientation from windows if screen is freezing by apps
    This patch prevents the app window being re-created repeatedly. With
    the original logic, if an app which is show-when-locked, which has a
    different orientation from the keyguard's, and which doesn't handle
    config changes, is launched when the screen is off, then it would be
    re-created repeatedly.
    
    The original logic prevents this issue when the display is frozen.
    However, if the screen is off, the display won't be frozen. So this
    change uses additional variables to detect if there are any apps re-
    creating their windows.
    
    Change-Id: I73d2e384c4ceeeb38d04d04f0665f83cc6fba7ae

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -3801,7 +3801,7 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     public int getOrientationLocked() {
-        if (mDisplayFrozen) {
+        if (mDisplayFrozen || mAppsFreezingScreen > 0) {
             if (mLastWindowForcedOrientation != ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED) {
                 if (DEBUG_ORIENTATION) Slog.v(TAG, "Display is frozen, return "
                         + mLastWindowForcedOrientation);
@@ -3826,8 +3826,8 @@ public class WindowManagerService extends IWindowManager.Stub
                     continue;
                 }
                 int req = win.mAttrs.screenOrientation;
-                if((req == ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED) ||
-                        (req == ActivityInfo.SCREEN_ORIENTATION_BEHIND)){
+                if ((req == ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED) ||
+                        (req == ActivityInfo.SCREEN_ORIENTATION_BEHIND)) {
                     continue;
                 }
 
@@ -3857,7 +3857,7 @@ public class WindowManagerService extends IWindowManager.Stub
                 }
                 if (DEBUG_ORIENTATION) Slog.v(TAG,
                         "No one is requesting an orientation when the screen is locked");
-                return mLastKeyguardForcedOrientation;
+                return mLastWindowForcedOrientation = mLastKeyguardForcedOrientation;
             }
         }
 
