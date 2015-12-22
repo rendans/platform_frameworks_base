CyanogenMod/android_frameworks_base

SystemUI : Add location tile from 12.1
Change-Id: I93fd420550d21161e14a817c29103cbdb358f63b

packages/SystemUI/src/com/android/systemui/statusbar/policy/LocationControllerImpl.java
@@ -37,6 +37,8 @@ import com.android.systemui.R;
 import java.util.ArrayList;
 import java.util.List;
 
+import cyanogenmod.providers.CMSettings;
+
 /**
  * A controller to manage changes of location related states and update the views accordingly.
  */
@@ -55,6 +57,7 @@ public class LocationControllerImpl extends BroadcastReceiver implements Locatio
     private StatusBarManager mStatusBarManager;
 
     private boolean mAreActiveLocationRequests;
+    private int mLastActiveMode;
 
     private ArrayList<LocationSettingsChangeCallback> mSettingsChangeCallbacks =
             new ArrayList<LocationSettingsChangeCallback>();
@@ -62,6 +65,11 @@ public class LocationControllerImpl extends BroadcastReceiver implements Locatio
 
     public LocationControllerImpl(Context context, Looper bgLooper) {
         mContext = context;
+
+        // Initialize last active mode. If state was off use the default high accuracy mode
+        mLastActiveMode = getLocationCurrentState();
+        if(mLastActiveMode == Settings.Secure.LOCATION_MODE_OFF)
+            mLastActiveMode = Settings.Secure.LOCATION_MODE_HIGH_ACCURACY;
 
         // Register to listen for changes in location settings.
         IntentFilter filter = new IntentFilter();
@@ -107,14 +115,59 @@ public class LocationControllerImpl extends BroadcastReceiver implements Locatio
             return false;
         }
         final ContentResolver cr = mContext.getContentResolver();
+
+        // Store last active mode if we are switching off
+        // so we can restore it at the next enable
+        if(!enabled) {
+            mLastActiveMode = getLocationCurrentState();
+        }
+
         // When enabling location, a user consent dialog will pop up, and the
         // setting won't be fully enabled until the user accepts the agreement.
         int mode = enabled
-                ? Settings.Secure.LOCATION_MODE_HIGH_ACCURACY : Settings.Secure.LOCATION_MODE_OFF;
+                ? mLastActiveMode : Settings.Secure.LOCATION_MODE_OFF;
         // QuickSettings always runs as the owner, so specifically set the settings
         // for the current foreground user.
         return Settings.Secure
                 .putIntForUser(cr, Settings.Secure.LOCATION_MODE, mode, currentUserId);
+    }
+
+    /**
+     * Enable or disable location in settings to a specific mode.
+     *
+     * <p>This will attempt to enable/disable every type of location setting
+     * (e.g. high and balanced power).
+     *
+     * <p>If enabling, a user consent dialog will pop up prompting the user to accept.
+     * If the user doesn't accept, network location won't be enabled.
+     *
+     * @return true if attempt to change setting was successful.
+     */
+    public boolean setLocationMode(int mode) {
+        int currentUserId = ActivityManager.getCurrentUser();
+        if (isUserLocationRestricted(currentUserId)) {
+            return false;
+        }
+        final ContentResolver cr = mContext.getContentResolver();
+        // When enabling location, a user consent dialog will pop up, and the
+        // setting won't be fully enabled until the user accepts the agreement.
+        // QuickSettings always runs as the owner, so specifically set the settings
+        // for the current foreground user.
+        return Settings.Secure.putIntForUser(cr, Settings.Secure.LOCATION_MODE,
+                mode, currentUserId);
+    }
+
+    /**
+     * Returns int corresponding to current location mode in settings.
+     */
+    public int getLocationCurrentState() {
+        int currentUserId = ActivityManager.getCurrentUser();
+        if (isUserLocationRestricted(currentUserId)) {
+            return Settings.Secure.LOCATION_MODE_OFF;
+        }
+        final ContentResolver cr = mContext.getContentResolver();
+        return Settings.Secure.getIntForUser(cr, Settings.Secure.LOCATION_MODE,
+                Settings.Secure.LOCATION_MODE_OFF, currentUserId);
     }
 
     /**
@@ -127,6 +180,14 @@ public class LocationControllerImpl extends BroadcastReceiver implements Locatio
         int mode = Settings.Secure.getIntForUser(resolver, Settings.Secure.LOCATION_MODE,
                 Settings.Secure.LOCATION_MODE_OFF, ActivityManager.getCurrentUser());
         return mode != Settings.Secure.LOCATION_MODE_OFF;
+    }
+
+    /**
+     * Check if advanced location tile is enabled in settings
+     */
+    public boolean isAdvancedSettingsEnabled() {
+        return CMSettings.Secure.getIntForUser(mContext.getContentResolver(),
+                CMSettings.Secure.QS_LOCATION_ADVANCED, 0, ActivityManager.getCurrentUser()) == 1;
     }
 
     /**

rendans/platform_frameworks_base

Signed-off-by: iceandfire <arhamjamal@gmail.com>
Conflicts:
	packages/SystemUI/res/layout/qs_settings.xml
	packages/SystemUI/res/values/cm_strings.xml

packages/SystemUI/src/com/android/systemui/statusbar/policy/LocationControllerImpl.java
@@ -55,6 +55,7 @@ public class LocationControllerImpl extends BroadcastReceiver implements Locatio
     private StatusBarManager mStatusBarManager;
 
     private boolean mAreActiveLocationRequests;
+    private int mLastActiveMode;
 
     private ArrayList<LocationSettingsChangeCallback> mSettingsChangeCallbacks =
             new ArrayList<LocationSettingsChangeCallback>();
@@ -62,6 +63,11 @@ public class LocationControllerImpl extends BroadcastReceiver implements Locatio
 
     public LocationControllerImpl(Context context, Looper bgLooper) {
         mContext = context;
+
+        // Initialize last active mode. If state was off use the default high accuracy mode
+        mLastActiveMode = getLocationCurrentState();
+        if(mLastActiveMode == Settings.Secure.LOCATION_MODE_OFF)
+            mLastActiveMode = Settings.Secure.LOCATION_MODE_HIGH_ACCURACY;
 
         // Register to listen for changes in location settings.
         IntentFilter filter = new IntentFilter();
@@ -107,14 +113,59 @@ public class LocationControllerImpl extends BroadcastReceiver implements Locatio
             return false;
         }
         final ContentResolver cr = mContext.getContentResolver();
+
+        // Store last active mode if we are switching off
+        // so we can restore it at the next enable
+        if(!enabled) {
+            mLastActiveMode = getLocationCurrentState();
+        }
+
         // When enabling location, a user consent dialog will pop up, and the
         // setting won't be fully enabled until the user accepts the agreement.
         int mode = enabled
-                ? Settings.Secure.LOCATION_MODE_HIGH_ACCURACY : Settings.Secure.LOCATION_MODE_OFF;
+                ? mLastActiveMode : Settings.Secure.LOCATION_MODE_OFF;
         // QuickSettings always runs as the owner, so specifically set the settings
         // for the current foreground user.
         return Settings.Secure
                 .putIntForUser(cr, Settings.Secure.LOCATION_MODE, mode, currentUserId);
+    }
+
+    /**
+     * Enable or disable location in settings to a specific mode.
+     *
+     * <p>This will attempt to enable/disable every type of location setting
+     * (e.g. high and balanced power).
+     *
+     * <p>If enabling, a user consent dialog will pop up prompting the user to accept.
+     * If the user doesn't accept, network location won't be enabled.
+     *
+     * @return true if attempt to change setting was successful.
+     */
+    public boolean setLocationMode(int mode) {
+        int currentUserId = ActivityManager.getCurrentUser();
+        if (isUserLocationRestricted(currentUserId)) {
+            return false;
+        }
+        final ContentResolver cr = mContext.getContentResolver();
+        // When enabling location, a user consent dialog will pop up, and the
+        // setting won't be fully enabled until the user accepts the agreement.
+        // QuickSettings always runs as the owner, so specifically set the settings
+        // for the current foreground user.
+        return Settings.Secure.putIntForUser(cr, Settings.Secure.LOCATION_MODE,
+                mode, currentUserId);
+    }
+
+    /**
+     * Returns int corresponding to current location mode in settings.
+     */
+    public int getLocationCurrentState() {
+        int currentUserId = ActivityManager.getCurrentUser();
+        if (isUserLocationRestricted(currentUserId)) {
+            return Settings.Secure.LOCATION_MODE_OFF;
+        }
+        final ContentResolver cr = mContext.getContentResolver();
+        return Settings.Secure.getIntForUser(cr, Settings.Secure.LOCATION_MODE,
+                Settings.Secure.LOCATION_MODE_OFF, currentUserId);
     }
 
     /**
@@ -127,6 +178,14 @@ public class LocationControllerImpl extends BroadcastReceiver implements Locatio
         int mode = Settings.Secure.getIntForUser(resolver, Settings.Secure.LOCATION_MODE,
                 Settings.Secure.LOCATION_MODE_OFF, ActivityManager.getCurrentUser());
         return mode != Settings.Secure.LOCATION_MODE_OFF;
+    }
+
+    /**
+     * Check if advanced location tile is enabled in settings
+     */
+    public boolean isAdvancedSettingsEnabled() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_LOCATION_ADVANCED, 0, ActivityManager.getCurrentUser()) == 1;
     }
 
     /**